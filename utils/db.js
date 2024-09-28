f2e1a9056b52113fe87e17ef5fff7229341ca9d7
Samuel25100 <samuelgselassie1@gmail.com>
2024-09-28 09:01:38 +0300

    add files for tasks-1

---
+++ utils/db.js
@@ -0,0 +1,41 @@
+import { MongoClient } from 'mongodb';
+
+class DBClient {
+
+	constructor() {
+		this.host = process.env.DB_HOST || 'localhost';
+		this.port = process.env.DB_PORT || 27017;
+		this.database = process.env.DB_DATABASE || 'files_manager';
+		const url = `mongodb://${this.host}:${this.port}/${this.database}`;
+		this.client = new MongoClient(url, { useUnifiedTopology: true });
+		this.ready = false;
+		this.client.connect()
+			.then(() => {
+				this.db = this.client.db(this.database);
+                        	this.users = this.db.collection('users');
+                        	this.files = this.db.collection('files');
+				this.ready = true;
+			})
+			.catch((err) => {
+				this.ready = false;
+				console.log(err);
+			});
+	}
+
+	isAlive() {
+		return Boolean(this.ready);
+	}
+
+	async nbUsers() {
+		const count = await this.users.countDocuments();
+		return count;
+	}
+
+	async nbFiles() {
+                const count = await this.files.countDocuments();
+                return count;
+        }
+}
+
+const dbClient = new DBClient();
+export default dbClient;
